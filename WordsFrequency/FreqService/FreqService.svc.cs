54410e1fefe1abf2963d15bb4a9454bfe45ae616
Nero257 <nero257work@mail.ru>
2021-03-15 00:32:40 +0300

    Ready third but not for big files

--- WordsFrequency/FreqService/FreqService.svc.cs
+++ WordsFrequency/FreqService/FreqService.svc.cs
@@ -1,37 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-//using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace WordsFrequencyMethod
+namespace FreqService
 {
-
-    public class FreqDictionary
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class FreqService : IFreqService
     {
-        
-        private static Dictionary<string, int> CreateFreqDictionary(string text)
+        public string GetData(int value)
         {
-            Dictionary<string, int> dict = new Dictionary<string, int>();
-
-            //Функция Split возвращает текст без символов, которые указаны в параметрах
-            string[] words = text.Split(new[] { '.', ',', '"', '\'', '!', '?', ' ', ':', ';', '-', '\r', '\n', '\0', '[', ']', ')', '(' }
-            //ToLower возвращает слово в нижнем регистре; 
-            //ToArray позволяет представить набор как массив, и работать с ним как с массивом.
-            , StringSplitOptions.RemoveEmptyEntries).Select(x => x.ToLower()).ToArray();
-
-            //Функция Distinct позволяет нам получить набор уникальных слов в общем наборе слов
-            string[] unique_words = words.Distinct().ToArray();
-            foreach (string word in unique_words)
-            {
-                int count = words.Count(x => x.Equals(word));
-                dict.Add(word, count);
-            }
-
-            return dict;
-
+            return string.Format("You entered: {0}", value);
         }
 
- /*       public static Dictionary<string, int> CreateFreqDictParallel(string text)
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public Dictionary<string, int> GetFrequencyDict(string text)
         {
             Dictionary<string, int> dict = new Dictionary<string, int>();
 
@@ -52,9 +52,6 @@ namespace WordsFrequencyMethod
             });
 
             return dict;
-
-        }*/
-
-
+        }
     }
 }
---
+++ WordsFrequency/WordsFrequencyNew/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using WordsFrequencyMethod;
+using System.Diagnostics;
+
+namespace WordsFrequency
+{
+    class Program
+    {
+        public static Dictionary<string, int> DictWithService(string text)
+        { 
+            var client = new WordsFrequencyNew.FDService.FreqServiceClient();
+            var result = client.GetFrequencyDict(text);
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Введите путь к файлу и нажмите Enter:");
+
+            //Считываем путь в переменную path через консоль 
+            string path = Console.ReadLine();
+
+            //Считываем весь текст из файла с указанным путем
+            string text = File.ReadAllText(path);
+
+            //Проверка на пустой файл
+            if (text != "")
+            {
+                Type t = typeof(FreqDictionary);
+                MethodInfo mi = t.GetTypeInfo().GetDeclaredMethod("CreateFreqDictionary");
+
+                //Добавляем Stopwatch класс
+                Stopwatch swatch = new Stopwatch();
+
+                //Reflection method Dictionary
+                swatch.Start();
+                //Dictionary<string, int> dict = (Dictionary<string, int>)mi.Invoke(null , new object[] {text});
+                swatch.Stop();
+                Console.WriteLine(swatch.ElapsedMilliseconds.ToString() + " - время приватного метода");
+
+                //Parallel method dictionary
+                swatch.Restart();
+                Dictionary<string, int> dict = DictWithService(text);
+                swatch.Stop();
+                Console.WriteLine(swatch.ElapsedMilliseconds.ToString() + " - время параллельного публичного метода");
+
+                //Запрашиваем путь для будущего файла
+                Console.WriteLine("Введите путь к папке файла:");
+                path = Console.ReadLine();
+
+                //Используем StreamWriter для создания и записи текста в файл
+                using (StreamWriter outputFile = new StreamWriter(path + "\\WordsFrequency.txt"))
+                {
+                    foreach (var elem in dict.OrderByDescending(x => x.Value))
+                    {
+                        //Файл будет сохранен после завершения работы программы
+                        outputFile.WriteLine(elem.Key + " " + elem.Value.ToString());
+                    }
+
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Файл пуст.");
+                Console.ReadLine();
+            }
+        }
+
+
+    }
+}
